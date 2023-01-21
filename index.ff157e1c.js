b92284dbf2bad6cb067940b59b8626502e029b2d
ANDRII SYNII <79702542+Angri154@users.noreply.github.com>
2023-01-21 22:07:20 +0200

    Add files via upload

---
+++ index.ff157e1c.js
@@ -0,0 +1,2 @@
+window.addEventListener("scroll",(e=>{let a=document.getElementById("navbar").classList,d="navbar_srolled";pageYOffset>400?a.add(d):a.remove(d)}));
+//# sourceMappingURL=index.ff157e1c.js.map
